30951838a7433e3beefafeed3a46497699e6b027
Aldo Castillo <caztle501@gmail.com>
2016-11-02 19:40:45 -0600

    Composer actualizado y AdminLTE 2 agregado
    Para acceder a las páginas de AdminLTE 2 hay que acceder a vendor/bower/admin-lte/index2.html en vez de web/

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'al3iJ6eC1Ow7eN5IY6ev9OZ1Eh3ey9',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
