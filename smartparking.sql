60404f6c78cff22c4ac4aa418748b46d9d6466e6
Arian Castillo <caztle501@gmail.com>
2016-12-13 20:51:12 -0600

    BD corregida

--- smartparking.sql
+++ smartparking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-12-2016 a las 16:47:07
+-- Tiempo de generación: 12-12-2016 a las 16:05:14
 -- Versión del servidor: 5.7.14
 -- Versión de PHP: 5.6.25
 
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `smartparking`
 --
-CREATE DATABASE IF NOT EXISTS `smartparking` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE `smartparking`;
 
 -- --------------------------------------------------------
 
@@ -30,11 +28,24 @@ USE `smartparking`;
 
 CREATE TABLE `parking` (
   `id_parking` int(11) NOT NULL,
-  `min_time` time(3) DEFAULT '00:00:00.000',
-  `max_time` time(3) DEFAULT '00:00:00.000',
-  `average_time` time(3) DEFAULT '00:00:00.000',
+  `min_time` time DEFAULT '00:00:00',
+  `max_time` time DEFAULT '00:00:00',
+  `average_time` time DEFAULT '00:00:00',
   `current_status` varchar(15) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Available'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `parking`
+--
+
+INSERT INTO `parking` (`id_parking`, `min_time`, `max_time`, `average_time`, `current_status`) VALUES
+(1, '00:00:00', '00:00:00', '00:00:00', 'Available'),
+(2, '00:00:00', '00:00:00', '00:00:00', 'Available'),
+(3, '00:00:00', '00:00:00', '00:00:00', 'Available'),
+(4, '00:00:00', '00:00:00', '00:00:00', 'Available'),
+(5, '00:00:00', '00:00:00', '00:00:00', 'Available'),
+(6, '00:00:00', '00:00:00', '00:00:00', 'Available'),
+(7, '00:00:00', '00:00:00', '00:00:00', 'Available');
 
 -- --------------------------------------------------------
 
@@ -45,10 +56,24 @@ CREATE TABLE `parking` (
 CREATE TABLE `record` (
   `id_record` int(10) UNSIGNED NOT NULL,
   `parking_id_parking` int(11) NOT NULL,
-  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `create_at` datetime DEFAULT NULL,
   `update_at` datetime DEFAULT NULL,
   `time_parking` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `record`
+--
+
+INSERT INTO `record` (`id_record`, `parking_id_parking`, `create_at`, `update_at`, `time_parking`) VALUES
+(1, 1, '2016-12-12 05:07:50', '2016-12-12 05:17:50', '00:10:00'),
+(2, 1, '2016-12-11 23:12:18', '2016-12-11 23:19:50', '00:07:32'),
+(3, 2, '2016-12-12 05:08:05', '2016-12-12 05:12:05', '00:04:00'),
+(4, 6, '2016-12-12 06:38:07', '2016-12-12 07:38:07', '01:00:00'),
+(5, 4, '2016-12-12 06:46:18', '2016-12-12 06:58:18', '00:12:00'),
+(6, 6, '2016-12-12 08:08:05', '2016-12-12 08:18:05', '00:10:00'),
+(7, 2, '2016-12-12 08:20:05', '2016-12-12 08:35:05', '00:15:00'),
+(8, 1, '2016-12-12 10:10:05', '2016-12-12 10:37:05', '00:27:00');
 
 -- --------------------------------------------------------
 
@@ -102,7 +127,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `record`
 --
 ALTER TABLE `record`
-  MODIFY `id_record` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_record` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
